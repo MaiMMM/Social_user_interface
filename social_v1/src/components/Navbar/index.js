b48cbb1b6ae59153ab7a1fdc554a6a71d12decb2
Chris He <h1119623520@gmail.com>
2021-07-16 16:07:13 -0400

    Create social_v1

---
+++ social_v1/src/components/Navbar/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import{FaBars} from 'react-icons/fa'
+import { 
+    Nav,
+    NavbarContainer,
+    NavLogo, 
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './NavbarElements'
+
+const Navbar = ({toggle}) => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>SOC!AL</NavLogo>
+                    <MobileIcon onClick ={toggle}>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="game">Games</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="sign up">Sign up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                    <NavBtnLink to ="/signin">Sign In </NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
---
+++ social_v1/src/components/Sidebar/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import{
+    SidebarContainer,
+    Icon,
+    CloseIcon,
+    SidebarWrapper,
+    SidebarMenu,
+    SidebarLink,
+    SideBtnWrap,
+    SidebarRoute
+} from './SidebarElement'
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen = {isOpen} onClick = {toggle}>
+            <Icon onClick = {toggle}>
+                <CloseIcon/>
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about" onClick = {toggle}>
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="discover" onClick = {toggle}>
+                        Discover
+                    </SidebarLink>
+                    <SidebarLink to="game" onClick = {toggle}>
+                        Games
+                    </SidebarLink>
+                    <SidebarLink to="sign up" onClick = {toggle}>
+                        Sign up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">
+                        Sign In
+                    </SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar;
