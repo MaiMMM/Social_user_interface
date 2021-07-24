1f9d1267d792cbcc817be0b682e9c7aabd98e66b
Chris He <h1119623520@gmail.com>
2021-07-23 21:42:39 -0400

    Multi_page_feature_added
    Added the ability to scroll down the webpage

---
+++ social_v1/src/components/InfoSection/data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Something topline',
+    headline: 'Something headline',
+    description: 'Something desription',
+    buttonLabel: 'Seomthing Label',
+    imgStart: false,
+    img: require('../../images/well-being.png'),
+    alt: 'Car',
+    dark: true,
+    primary:true,
+    darkText: false
+};
